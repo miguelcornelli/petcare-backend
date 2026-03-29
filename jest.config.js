5851402ac44b3a5ce4034821df4621dad090f038
Miguel Cornelli <cornellimiguel@outlook.com>
2026-03-29 18:43:23 -0300

    fix: convert jest.config to JS and fix uuid ESM transform

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
-export default {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/__tests__'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {}],
   },
   transformIgnorePatterns: [
     'node_modules/(?!(uuid)/)',
@@ -13,3 +17,5 @@ export default {
     global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   },
 }
+
+module.exports = config
